07f5bc4c3009753d8b1fe7240fa52a69d391227d
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-07-19 00:06:37 +0300

    test husky

--- src/features/profile/userProfile/ui/UserProfile.tsx
+++ src/features/profile/userProfile/ui/UserProfile.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+
 import { useGetPostsQuery } from '../service/posts'
+
 import cls from './UserProfile.module.scss'
 import { UserProfileContent } from './UserProfileContent/UserProfileContent'
 import { UserProfileHeader } from './UserProfileHeader/UserProfileHeader'
+
 import { useGetProfileQuery } from 'features/profile/profileSetting/generalInformation/service/profile'
 import { getUserId } from 'shared/hoc'
 import { useAppSelector } from 'shared/hooks/useAppSelector'
 import { Loader } from 'shared/ui/Loader/Loader'
+
 export const UserProfile = () => {
   const userId = useAppSelector(getUserId)
   const { data: profileData, isLoading: profileLoading } = useGetProfileQuery()
