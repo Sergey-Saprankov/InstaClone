ca77404e5801367a75e4a1df9095ca8a9c46392a
zemosamurai <zemosamurai@gmail.com>
2023-07-22 16:10:56 +0300

    refactor: migration user entities to folder modules

--- src/features/profile/userProfile/ui/UserProfile.tsx
+++ src/features/profile/userProfile/ui/UserProfile.tsx
@@ -6,14 +6,14 @@ import cls from './UserProfile.module.scss'
 import { UserProfileContent } from './UserProfileContent/UserProfileContent'
 import { UserProfileHeader } from './UserProfileHeader/UserProfileHeader'
 
-import { useGetProfileQuery } from 'features/profile/profileSetting/generalInformation/service/profile'
+import { useGetUserInfoQuery } from 'modules/user/service/user'
 import { getUserId } from 'shared/hoc'
 import { useAppSelector } from 'shared/hooks/useAppSelector'
 import { Loader } from 'shared/ui/Loader/Loader'
 
 export const UserProfile = () => {
   const userId = useAppSelector(getUserId)
-  const { data: profileData, isLoading: profileLoading } = useGetProfileQuery()
+  const { data: profileData, isLoading: profileLoading } = useGetUserInfoQuery()
   const { data: postsData, isLoading: postsLoading } = useGetPostsQuery(userId, {
     skip: !userId,
   })
