2ce63a675a26df4b7c2d37db215f3ccb3ed1ec15
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-07-31 18:55:17 +0300

    feat: implement fetching posts by infinite scrolling

--- src/features/profile/userProfile/ui/UserProfileContent/UserProfileContent.tsx
+++ src/features/profile/userProfile/ui/UserProfileContent/UserProfileContent.tsx
@@ -2,89 +2,92 @@ import React, { FC, memo, useCallback, useEffect, useState } from 'react'
 
 import { useInView } from 'react-intersection-observer'
 
-import { IPost, PostsResponse } from '../../service/types'
+import { PostsResponse } from '../../service/types'
 
+import { PostsLoader } from './PostsLoader'
 import cls from './UserProfileContent.module.scss'
 
 import { Post } from 'modules/post'
-import { getUserId } from 'shared/hoc'
-import { useAppSelector } from 'shared/hooks/useAppSelector'
 import { getCurrentPostData } from 'shared/lib/getCurrentPostData/getCurrentPostData'
 import { Card } from 'shared/ui/Card/Card'
 
 interface IUserProfileContentProps {
   data?: PostsResponse
+  changePostsPage: (nextPage: number) => void
+  isFetchingPosts: boolean
 }
 
-export const UserProfileContent: FC<IUserProfileContentProps> = memo(({ data }) => {
-  const [currentId, setCurrentId] = useState<null | number>(null)
-  const [step, setStep] = useState<number>(0)
-  const [page, setPage] = useState<number>(1)
+export const UserProfileContent: FC<IUserProfileContentProps> = memo(
+  ({ data, changePostsPage, isFetchingPosts }) => {
+    const [currentId, setCurrentId] = useState<null | number>(null)
+    const [step, setStep] = useState<number>(0)
 
-  if (!data) return null
+    const [triggeredPage, setTriggeredPage] = useState(1)
 
-  const { items } = data
-  const index = items.findIndex(item => item.id === currentId)
+    if (!data) return null
 
-  const currentPostData = getCurrentPostData(items, index, step)
+    const { items } = data
+    const index = items.findIndex(item => item.id === currentId)
 
-  const userId = useAppSelector(getUserId)
+    const currentPostData = getCurrentPostData(items, index, step)
 
-  const { ref, inView } = useInView()
+    const { ref, inView } = useInView()
 
-  const getCurrentPostId = useCallback((id: number | null) => {
-    setCurrentId(id)
-  }, [])
+    const getCurrentPostId = useCallback((id: number | null) => {
+      setCurrentId(id)
+    }, [])
 
-  const onChangeStep = useCallback((value: number) => {
-    setStep(value)
-  }, [])
+    const onChangeStep = useCallback((value: number) => {
+      setStep(value)
+    }, [])
 
-  useEffect(() => {
-    // console.log(page)
-    // useGetPostsQuery({ userId, page });
-  }, [page])
+    useEffect(() => {
+      changePostsPage(triggeredPage)
+    }, [triggeredPage])
 
-  useEffect(() => {
-    if (inView) {
-      setPage(prevPage => prevPage + 1)
-    }
-  }, [inView])
+    useEffect(() => {
+      if (inView) {
+        setTriggeredPage(prevState => prevState + 1)
+      }
+    }, [inView])
 
-  return (
-    <>
-      <div className={cls.UserProfileContent}>
-        {data &&
-          data.items.map(({ id, images, description }) => {
-            const mutableArray = [...images]
+    return (
+      <>
+        <div className={cls.UserProfileContent}>
+          {data &&
+            data.items.map(({ id, images, description }) => {
+              const mutableArray = [...images]
 
-            mutableArray.sort((a, b) => a.width - b.width)
-            const src = mutableArray[1].url
+              mutableArray.sort((a, b) => a.width - b.width)
+              const src = mutableArray[1].url
 
-            return (
-              <Card
-                id={id}
-                key={id}
-                callBack={getCurrentPostId}
-                src={src}
-                alt={description ? description : 'post'}
-              />
-            )
-          })}
-        {currentPostData && (
-          <Post
-            currentIndex={currentPostData.currentIndex}
-            step={step}
-            endIndex={currentPostData.lastPostIndex}
-            onChangeStep={onChangeStep}
-            alt={currentPostData.alt}
-            src={currentPostData.src}
-            callBack={getCurrentPostId}
-            currentId={currentPostData.id}
-          />
-        )}
-      </div>
-      <div ref={ref}></div>
-    </>
-  )
-})
+              return (
+                <Card
+                  id={id}
+                  key={id}
+                  callBack={getCurrentPostId}
+                  src={src}
+                  alt={description ? description : 'post'}
+                />
+              )
+            })}
+
+          {currentPostData && (
+            <Post
+              currentIndex={currentPostData.currentIndex}
+              step={step}
+              endIndex={currentPostData.lastPostIndex}
+              onChangeStep={onChangeStep}
+              alt={currentPostData.alt}
+              src={currentPostData.src}
+              callBack={getCurrentPostId}
+              currentId={currentPostData.id}
+            />
+          )}
+        </div>
+        <div className={cls.loaderContainer}>{isFetchingPosts && <PostsLoader />}</div>
+        <div ref={ref}></div>
+      </>
+    )
+  }
+)
