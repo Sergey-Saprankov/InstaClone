c1db0e5899838b4b2a8425c6977b5c6eebee1f8a
Chernik1991 <5059585@mail.ru>
2023-08-21 13:17:07 +0300

    feat: Stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: 'stylelint-config-clean-order',
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'selector-class-pattern': null,
+  },
 }
