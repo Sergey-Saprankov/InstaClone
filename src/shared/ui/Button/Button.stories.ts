d4bfc8c51e6f1e26ea6189e5b3500de42b88df88
Chernik1991 <5059585@mail.ru>
2023-08-28 12:30:12 +0300

    fix: StoryBook start, text and button

---
+++ src/shared/ui/Button/Button.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  args: {
+    children: 'Primary Button',
+    disabled: false,
+    size: ButtonSize.XXl,
+    theme: ButtonTheme.PRIMARY,
+    // fullWidth: true,
+  },
+}
+export const Light: Story = {
+  args: {
+    children: 'Light Button',
+    disabled: false,
+    size: ButtonSize.XXl,
+    theme: ButtonTheme.LIGHT,
+    // fullWidth: true,
+  },
+}
+//
+// export const Secondary: Story = {
+//   args: {
+//     variant: 'secondary',
+//     children: 'Secondary Button',
+//     disabled: false,
+//   },
+// }
+// export const Tertiary: Story = {
+//   args: {
+//     variant: 'tertiary',
+//     children: 'Tertiary Button',
+//     disabled: false,
+//   },
+// }
+// export const Link: Story = {
+//   args: {
+//     variant: 'link',
+//     children: 'Tertiary Button',
+//     disabled: false,
+//   },
+// }
+//
+// export const FullWidth: Story = {
+//   args: {
+//     variant: 'primary',
+//     children: 'Full Width Button',
+//     disabled: false,
+//     fullWidth: true,
+//   },
+// }
