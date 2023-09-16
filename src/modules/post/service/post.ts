6865cdf0eb2dc8b51a4e84633aacb26ff4d8ce8a
Sergey <101936909+Sergey-Saprankov@users.noreply.github.com>
2023-09-16 16:13:24 +0300

    Dev (#74)
    * refactor: decomposition
    
    * refactor: delete dublicate code
    
    * fix: paginate in canvas, add selectors, fix logic
    
    * delete: filter selector
    
    * Fix create post (#54)
    
    * refactor: add post image slider
    
    * refactor: add post image slider
    
    * refactor: add post image slider
    
    * feat: AutoRenewal and Current Subscription HardCore (#46)
    
    * feat: AutoRenewal and Current Subscription HardCore
    
    * fix: AutoRenewal and Current Subscription HardCore
    
    * fix: AutoRenewal and Current Subscription HardCore
    
    * refactor: fix
    
    * refactor: fix
    
    ---------
    
    Co-authored-by: Sergey-Saprankov <sergey-saprankov@mail.ru>
    
    * fix: Google Auth finish
    
    * fix: Google Auth finish (#58)
    
    * refactor: fix post user endpoint
    
    * refactor: fix post user endpoint
    
    * fix: delete user button (#60)
    
    * fix: googleAuth add headers
    
    * feat: set last uploaded post id to store after getting post portion
    
    * fix: set id last uploaded post when intersect
    
    * Sign up add feature (#63)
    
    * feat: terms, privacy policy
    
    * feat: terms, privacy policy
    
    * feat: terms, privacy policy
    
    * Sign up add feature (#65)
    
    * feat: terms, privacy policy
    
    * feat: terms, privacy policy
    
    * feat: terms, privacy policy
    
    * feat: terms, privacy policy
    
    * fix: googleAuth add in registration button
    
    * fix: sending idLastUploadedPost in uri params
    
    * feat: delete post by optimistic update
    
    * refactor: photo size
    
    * refactor: fix datepicker (#69)
    
    * Fix datepicker (#70)
    
    * refactor: fix datepicker
    
    * refactor: fix datepicker
    
    * Fix datepicker (#72)
    
    * refactor: fix datepicker
    
    * refactor: fix datepicker
    
    * refactor: fix datepicker
    
    * fix: show added post (#73)
    
    ---------
    
    Co-authored-by: zemosamurai <zemosamurai@gmail.com>
    Co-authored-by: Chernik Maksim <94241036+Chernik1991@users.noreply.github.com>
    Co-authored-by: Chernik1991 <5059585@mail.ru>
    Co-authored-by: grigorydobrenko <grigorydobrenko97@gmail.com>

--- src/modules/post/service/post.ts
+++ src/modules/post/service/post.ts
@@ -2,6 +2,7 @@ import { postsApi } from '../../../features/profile/userProfile'
 
 import { IUpdatePostRequest, ResponsePost } from './types'
 
+import { postsApi } from 'features/profile/userProfile'
 import { baseAPI } from 'shared/api/baseAPI'
 
 const post = baseAPI.injectEndpoints({
@@ -31,7 +32,6 @@ const post = baseAPI.injectEndpoints({
           patchResult.undo()
         }
       },
-      invalidatesTags: ['Posts'],
     }),
     updatePost: build.mutation<void, IUpdatePostRequest>({
       query: ({ postId, description }) => ({
